8fd6f3fdea516917b519249d27e2bb96b2a87504
Victor14791012 <victor.carvalhok23@gmail.com>
2024-05-17 14:20:12 -0400

    17/5
    volta as aulas

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,23 @@
+import Image from "next/image"
+import styles from "./page.module.css"
 
 export default async function Home() {
-
+  const response = await fetch("https://fakestoreapi.com/products/")
+  const data = await response.json();
+  
   return (
-    <main>
+    <main className={styles.main}>
+      {data.map((produto)=>
+      <div key={produto.id} className={styles.card} >
+      <p>{produto.title}</p>
+      <Image
+      width={100}
+      height={100}
+      src={produto.image} />
+
+      </div>
+      
+      )}
     </main>
   );
 }
